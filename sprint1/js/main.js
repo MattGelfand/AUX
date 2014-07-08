bf96ac22c5a0500fd6a9b89377f70d4d89fd9048
Matt Gelfand <matthew.gelfand@gmail.com>
2014-07-08 14:14:40 -0400

    modified sprint1

--- sprint1/js/main.js
+++ sprint1/js/main.js
@@ -1,84 +1,111 @@
-(function() {	
+function getHTTPObject() {
+    var xhr;
+    if (window.XMLHttpRequest) { //check for support
+        //if it's supported, use it because it's better
+        xhr = new XMLHttpRequest();
+    } else if (window.ActiveXObject) { //check for ie 6
+        //save it to the xhr variable
+        xhr = new ActiveXObject('Msxml2.XMLHTTP');
+    }
 
-var contacts = {
-"addressbook" : [
-		{
-		"name": "Richard",
-		"email": "richard@freshtilledsoil.com",
-		},
+    return xhr;
+}
 
-		{
-		"name": "Kelly",
-		"email": "kelly@freshtilledsoil.com",
-		},
+//define the ajax call
+function ajaxCall(dataURL, outputElement, callback) {
 
-		{
-		"name": "Mark",
-		"email": "mark.grambau@freshtilledsoil.com",
-		},
+    var request = getHTTPObject();
+    outputElement.innerHTML = "Loading...";
 
-		{
-		"name": "Mike",
-		"email": "michael.connors@freshtilledsoil.com",
-		},
+    request.onreadystatechange = function() {
+        setTimeout(function(){
+            if(request.readyState === 4 && request.status === 200) {
+            var contacts = JSON.parse(request.responseText);
+            if (typeof callback === 'function') {
+                callback(contacts);
+                } // end function check
 
-		{
-		"name": "Sarah",
-		"email": "sarah@freshtilledsoil.com",
-		}	
-	]
-};
+            } // end ajax status check
+        },500);
+        } // end onreadystatechange 
+        
+
+        request.open('GET', dataURL, true);
+        request.send(null);
+}
+
+
+
+
+(function() {   
+
 
 var searchForm = document.getElementById('search-form');
 var searchField = document.getElementById('q');
 var getAllButton = document.getElementById('get-all');
-var count = contacts.addressbook.length;
 var target = document.getElementById('output');
 
-
 var adr = {
-	search : function(event) {
-		var searchValue = searchField.value.toLowerCase(),
-			i;
+    search : function(event) {
+        var output = document.getElementById('output');
+        ajaxCall('data/contacts.json', output, function (data) {
 
-		event.preventDefault();
+            adr.hasCompletedLoading = true;
 
-		target.innerHTML = " ";
+            var searchValue = searchField.value.toLowerCase(),
+                addrBook = data.addressBook,
+                count = addrBook.length,
+                i;
 
-		if(count > 0 && searchValue !== " ") {
-			for(i = 0; i < count; i = i + 1) {
-				var obj = contacts.addressbook[i],
-				name = obj.name.toLowerCase(),
-				isitFound = obj.name.indexOf(searchValue);
-				if(isitFound !== -1) {
-					target.innerHTML += '<p>' + obj.name + ', <a href="mailto:' + obj.email + '">' + obj.email +'</a></p>';
+            event.preventDefault();
 
-				} // end if
-			} // end for loop
-		} // end count stack
-	}, 
-	getAllContacts : function () {
-		var i;
-		target.innerHTML = " ";
-		if(count > 0) {
-			for(i = 0; i < count; i = i + 1) {
-				var obj = contacts.addressbook[i];
-					target.innerHTML += '<p>' + obj.name + ', <a href="mailto:' + obj.email + '">' + obj.email +'</a></p>';
-			} // end for loop
-		} // end count check
-	}, 
-	setActiveSelection : function() {
-		this.parentNode.setAttribute('class', 'active');
-	},
-	removeAttribute : function() {
-		this.parentNode.removeAttribute('class');
-	},
-	addHoverClass : function() {
-		searchForm.setAttribute('class', 'hovering');
-	},
-	removeHoverClass : function() {
-		searchForm.removeHoverClass('class');
-	},
+            target.innerHTML = " ";
+            var htmlToWrite = "";
+            if(count > 0 && searchValue !== "") {
+                htmlToWrite = "";
+                for(i = 0; i < count; i = i + 1) {
+
+                    obj = addrBook[i],
+
+                    lowercaseName = obj.name.toLowerCase(),
+                    isitFound = lowercaseName.indexOf(searchValue);
+
+                    if(isitFound !== -1) {
+                        htmlToWrite += '<p>' + obj.name + ', <a href="mailto:' + obj.email + '">' + obj.email +'</a></p>';
+                    } // end if
+                } // end for loop
+            } // end count stack
+            target.innerHTML = htmlToWrite;
+        }); // end ajax call
+    }, 
+    getAllContacts : function () {
+        var output = document.getElementById('output');
+        ajaxCall('data/contacts.json', output, function (data) {
+            var addrBook = data.addressBook,
+            count = addrBook.length,
+            i;
+        target.innerHTML = " ";
+        if(count > 0) {
+            for(i = 0; i < count; i = i + 1) {
+                var obj = addrBook[i];
+                    target.innerHTML += '<p>' + obj.name + ', <a href="mailto:' + obj.email + '">' + obj.email +'</a></p>';
+                } // end for loop
+            } // end count check'
+        }); // end ajax call
+    }, 
+    setActiveSelection : function() {
+        this.parentNode.setAttribute('class', 'active');
+    },
+    removeAttribute : function() {
+        this.parentNode.removeAttribute('class');
+    },
+    addHoverClass : function() {
+        searchForm.setAttribute('class', 'hovering');
+    },
+    removeHoverClass : function() {
+        searchForm.removeAttribute('class');
+    },
+    hasCompletedLoading : false
 } // end adr object
 
 searchField.addEventListener('keyup', adr.search, false);
@@ -90,21 +117,6 @@ searchForm.addEventListener('mouseout', adr.removeHoverClass, false);
 searchForm.addEventListener('submit', adr.search, false);
 
 })();
-
-function getHTTPObject() {
-	var xhr;
-	if (window.XMLHttpRequest) {
-		xhr = new XMLHttpRequest();
-	} else if (window.ActiveXObject) {
-		xhr = new ActiveXObject('Msxml2.XMLHTTP');
-	}
-
-	return xhr;
-}
-
-var request = getHTTPObject();
-request.open('GET', 'data/contacts.json', true);
-request.send(null);
 
 
 
